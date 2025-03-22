b5ec6f217fef801d3e822e73beb31ce1bf0fd5b9
justanuragmaurya <justanuragmaurya@gmail.com>
2025-03-22 15:00:30 +0530

    added meta tags

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <head>
+      <title>Sol Devta</title>
+      <link rel="shortcut icon" href="/image.png" type="image/x-icon" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@digitalocean" />
+      <meta name="twitter:title" content="Sol Devta" />
+      <meta
+        name="twitter:description"
+        content="Solana faucet for the devnet"
+      />
+      <meta
+        name="twitter:image"
+        content="https://eb7cw7lpb6.ufs.sh/f/y8KqGYrvxK6gAvttIlZ0RO8TyGqmZWDINApCnd1J6wFe3tSK"
+      />
+    </head>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
